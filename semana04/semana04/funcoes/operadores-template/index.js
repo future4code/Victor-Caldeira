8df019cd1cf2fd2d537360253b3e99c1a4c62f81
VictorCCal22 <81269490+VictorCCal22@users.noreply.github.com>
2021-04-26 17:38:11 -0300

    aula-funçoes

---
+++ semana04/semana04/funcoes/operadores-template/index.js
@@ -0,0 +1,93 @@
+console.log('Olá, Paiva!')
+
+// EXERCICIO 1 ------------------------------------------
+
+
+// a) Sera impresso o o valor da resposta da variavel multiplicado pela 
+// função: 2*5 e 10*5
+
+// b) Console.log é ima função para imprimir, sem ele não seria impresso
+// o resultado;
+
+
+
+// EXERCICIO 2 ------------------------------------------
+
+
+// a) let 0 indica posição do primeiro arrey; i<2 indica quantos array
+// quero que seja lido; sera impresso o primeiro e segundo array, darvas e caio; 
+
+// b) seria impresso o primeiro e segundo array, amanda e caio
+
+
+
+
+// EXERCICIO 3 ------------------------------------------
+
+
+// O restante de x sera multiplicado por x e o retorno será
+// inserido no arreyFinal;
+
+
+
+// EXERCICIO 4 ------------------------------------------
+
+
+// a) 
+
+//function sobreMim () {
+//    console.log ("Eu sou Victor," + "tenho 28 anos, " + "moro em Belo Horizonte" + " e sou estudante de Dev Labenu e PG na Fac Arnaldo.")
+//}
+//sobreMim ()
+
+// b) 
+
+
+
+
+
+// EXERCICIO 5 ------------------------------------------
+
+
+// a)
+
+/* function numeros (numero1, numero2) {
+    const somaNumeros = numero1 + numero2
+    return somaNumeros
+}
+
+const resultadoSoma = numeros (1, 3)
+
+console.log (resultadoSoma) */
+
+// b) 
+
+
+
+
+
+// EXERCICIO 6 ------------------------------------------
+
+
+//const array = [10, 23, 45, 78, 90, 52, 35, 67, 84, 22]
+
+
+// a)
+
+
+
+
+// b)
+
+
+
+// c)
+
+
+
+// d)
+
+
+
+
+
