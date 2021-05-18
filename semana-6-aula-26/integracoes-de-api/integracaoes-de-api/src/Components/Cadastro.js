fc5323be0e66145aceb174798922ba2ac5078573
VictorCCal22 <81269490+VictorCCal22@users.noreply.github.com>
2021-05-18 19:10:51 -0300

    intgração basica de API - sistema de cadastro de usuarios

---
+++ semana-6-aula-26/integracoes-de-api/integracaoes-de-api/src/Components/Cadastro.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+const BASE_URL =
+"https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/:id"
+
+export default class App extends React.Component {
+  
+
+  render() {
+   
+    return (
+      <div>
+        <h1>LabenuSers </h1>
+
+        <button onClick={}>Trocar de Página</button>
+
+        <h2> Lista de Usuario</h2>
+
+        <li></li>
+       
+      </div>
+    );
+  }
+}
