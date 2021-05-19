c111a41f7f92ade6330c42650a8d6394143740ba
VictorCCal22 <81269490+VictorCCal22@users.noreply.github.com>
2021-05-19 18:15:07 -0300

    integração de api - parte 2

--- semana-6-aula-26/integracoes-de-api/integracaoes-de-api/src/App.js
+++ semana-6-aula-26/integracoes-de-api/integracaoes-de-api/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import axios from "axios";
+import CriarUsuario from "./Components/CriarUsuario";
+import ListaUsuario from "./Components/ListaUsuario";
 
 
 const BASE_URL =
@@ -7,85 +8,40 @@ const BASE_URL =
 
 export default class App extends React.Component {
   state = {
-    listaUsuario: [],
-    inputName: "",
-    inputEmail: "",
+    telaAtual: "lista",
+
+
   };
 
-  componentDidMount() {
-    this.getListaUsuario();
+  telaAtual = () => {
+    switch (this.state.telaAtual) {
+      case "cadastro":
+        return <CriarUsuario cadastroUsuario={this.usuarioCadastrado} />
+      case "lista":
+        return <ListaUsuario usuarioCadastrado={this.cadastroUsuario} />
+      default:
+        return <p>Recarregue a página - Página não encontrada - Acesso restrito.</p>
+    }
   }
 
-  handleName = (event) => {
-    this.setState({ inputName: event.target.value });
-  };
+  cadastroUsuario = () => {
+    this.setState({ telaAtual: "cadastro" })
+  }
 
-  handleEmail = (event) => {
-    this.setState({ inputEmail: event.target.value });
-  };
+  usuarioCadastrado = () => {
+    this.setState({ telaAtual: "lista" })
+  }
 
-  getListaUsuario = () => {
-    const header = {
-      headers: {
-        Authorization: "victor-cesar-paiva"
-      }
-    };
 
-    axios
-      .get(BASE_URL, header)
-      .then((res) => {
-        this.setState({ listaUsuario: res.data.result.list });
-      })
-      .catch((err) => {
-        alert("Não foi possivel criar cadastro. Tente novamente.");
-      });
-  };
-
-  createListaUsuario = () => {
-    const header = {
-      headers: {
-        Authorization: "victor-cesar-paiva"
-      }
-    };
-
-    const body = {
-      name: this.state.inputName,
-      email: this.state.inputEmail,
-    };
-
-    axios
-      .post(BASE_URL, body, header)
-      .then(() => {
-        alert("O usuario foi criado com sucesso");
-        this.setState({ inputName: "" });
-        this.setState ({ inputEmail: ""});
-        this.getListaUsuario();
-      })
-      .catch((err) => {
-        alert();
-      });
-  };
 
   render() {
-    const ListaUsuarioComponents = this.state.listaUsuario.map((play) => {
-      return <li key={play.id}>{play.name} - {play.email} </li>;
-    });
+
 
     return (
+
       <div>
-        <h1>LabenuSers </h1>
+        {this.telaAtual()}
 
-        <button onClick={this.createListaUsuario}>Trocar de Página</button>
-
-        <h2> Criar Usuário</h2>
-
-        <input placeholder={"insira seu nome"} value={this.state.inputName} onChange={this.handleName} />
-        <input placeholder={"insira seu E-mail"} value={this.state.inputEmail} onChange={this.handleEmail} />
-
-        <button onClick={this.createListaUsuario}>Criar</button>
-
-        {ListaUsuarioComponents}
-        
       </div>
     );
   }
