b77f0a62e63ee6ba552737960dbd54c79b95f02d
VictorCCal22 <81269490+VictorCCal22@users.noreply.github.com>
2021-06-23 18:31:20 -0300

    elaborando pagina de login

--- semana-12-labeddit/semana-12-labeddit/semana-12-labeddit/src/pages/LoginPage/LoginPage.js
+++ semana-12-labeddit/semana-12-labeddit/semana-12-labeddit/src/pages/LoginPage/LoginPage.js
@@ -1,10 +1,62 @@
 import React from "react";
+import { ScreenContainer, LogoImage, InputContainer } from "./styled"
+import logo from "../../imagens-assents/pesquisa.png"
+import { TextField } from "@material-ui/core";
+import useForm from "../../hooks/useForms";
+import { Button } from "@material-ui/core";
+
 
 const LoginPage = () => {
+
+    const [form, onChange, clear] = useForm({ email: "", password: "" })
+
+    const onSubmitForm = () => {
+
+    }
+
     return (
-        <div>
-            <h1> LoginPage </h1>
-        </div>
+        <ScreenContainer>
+            <LogoImage scr={logo} />
+            <h1> LoginPage (era para ter uma imagem aqui) </h1>
+            <InputContainer>
+                <form onSubmit={onSubmitForm} >
+                    <TextField
+                        name={"email"}
+                        value={form.email}
+                        onChange={onChange}
+                        label={"Email"}
+                        variant={"outlined"}
+                        fullWidth
+                        required
+                        margin={"dense"}
+                        type={"email"} />
+
+                    <TextField
+                        name={"password"}
+                        value={form.password}
+                        onChange={onChange}
+                        label={"Password"}
+                        variant={"outlined"}
+                        fullWidth
+                        required
+                        margin={"dense"}
+                        type={"password"} />
+
+                    <Button
+                        color={"primary"}
+                        margin={"dense"}
+                        type={"submit"}
+                        fullWidth  > Entrar </Button>
+
+                    <Button
+                        color={"primary"}
+                        margin={"dense"}
+                        type={"submit"}
+                        fullWidth  > Cadastrar </Button>
+                        
+                </form>
+            </InputContainer>
+        </ScreenContainer>
     )
 }
 
